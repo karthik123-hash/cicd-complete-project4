58a2be7dee4260da5da0e344750cdae6558760f3
karthik123-hash <karthikdputhran@gmail.com>
2025-08-12 09:37:29 +0530

    node and role name updated

--- eks/main.tf
+++ eks/main.tf
@@ -98,7 +98,7 @@ resource "aws_eks_cluster" "project4" {
 
 resource "aws_eks_node_group" "project4" {
   cluster_name    = aws_eks_cluster.project4.name
-  node_group_name = "project4-node-group-v2"
+  node_group_name = "project4-node-group-v3"
   node_role_arn   = aws_iam_role.project4_node_group_role.arn
   subnet_ids      = aws_subnet.project4_subnet[*].id
 
@@ -117,7 +117,7 @@ resource "aws_eks_node_group" "project4" {
 }
 
 resource "aws_iam_role" "project4_cluster_role" {
-  name = "project4-cluster-role-v2"
+  name = "project4-cluster-role-v3"
 
   assume_role_policy = <<EOF
 {
@@ -141,7 +141,7 @@ resource "aws_iam_role_policy_attachment" "project4_cluster_role_policy" {
 }
 
 resource "aws_iam_role" "project4_node_group_role" {
-  name = "project4-node-group-role-v2"
+  name = "project4-node-group-role-v3"
 
   assume_role_policy = <<EOF
 {
